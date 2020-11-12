1ed78672580321bee5469ac5ab70e0df7c765cd8
jornelas4 <jornelas4@my.wctc.edu>
2020-11-11 20:52:50 -0600

    Validation

--- validation.js
+++ validation.js
@@ -1,16 +1,45 @@
 $(document).ready(function () {
     // Object containing the validation rules
     var myRules =
-        {};
+        {
+            firstName:
+                {
+                    required: true,
+
+                },
+            grade:
+                {
+                    min: 5,
+                    max: 8,
+                    digits: true
+                }
+
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+                {
+                    required: "Please enter a name!!!",
+
+                },
+            grade:
+                {
+                    min: "Grade not available too small. ",
+                    max: "Grade not available too big. ",
+
+                }
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
     $("form").validate(
-        {}
+        {
+            submitHandler: runMyProgram(),
+            rules: myRules,
+            messages: myMessages
+        }
     );
 
     function runMyProgram() {
@@ -18,6 +47,10 @@ $(document).ready(function () {
         // state, for example, "You have registered Jane for grade 6 camp!"
         // Use the name and grade the user provided in the form.
 
+        var name = $("#firstName").val();
+        var grade = $("#grade").val();
+
+        $("#message").text(`${name} ${grade}`);
 
     }
 
